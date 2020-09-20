582b05b61cd472928835fdd919afbc47be17d41f
JP Kramer <jpkramer707_github@mm.st>
2020-09-20 01:28:47 +0000

    Less static

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,57 @@
 import Head from 'next/head'
 
 export default function Home() {
-  var bits = [];
-  var bits2 = [];
-  var bits3 = [];
-  var rolling = 50;
-  var eccentrism = 0;
-  for (var x=0; x<500; x++) {
-    eccentrism = parseInt((Math.random() * 30)) * (Math.random() < 0.5 ? -1 : 1);
-    rolling = Math.max(0, Math.min(100, rolling + eccentrism));
-    bits.push(rolling);
+  const streamCount = 3;
+  const colors = [
+    [
+      [255,255,255]
+    ],
+    [
+      [255,255,0],
+      [0,0,255]
+    ],
+    [
+      [255,0,0],
+      [0,255,0],
+      [0,0,255]
+    ],
+    [
+      [255,0,0],
+      [0,255,0],
+      [0,0,255],
+      [0,127,127]
+    ],
+    [
+      [255,0,0],
+      [0,255,0],
+      [0,0,255],
+      [0,127,127]
+      [127,127,0]
+    ]
+  ];
+  const makeBits = (count, min, max) => {
+    const bits = [];
+    var rolling = (max-min)/2;
+    var eccentrism = 0;
+    for (var x=0; x<count; x++) {
+      eccentrism = parseInt((Math.random() * 30)) * (Math.random() < 0.5 ? -1 : 1);
+      rolling = Math.max(min, Math.min(max, rolling + eccentrism));
+      bits.push(rolling);
+    }
+    return bits;
   }
-
-  for (var x=0; x<500; x++) {
-    eccentrism = parseInt((Math.random() * 30)) * (Math.random() < 0.5 ? -1 : 1);
-    rolling = Math.max(0, Math.min(100, rolling + eccentrism));
-    bits2.push(rolling);
+  const bits = [];
+  for (var x=0; x<streamCount; x++) {
+    bits.push(makeBits(500, 0, 100));
   }
-
-  for (var x=0; x<500; x++) {
-    eccentrism = parseInt((Math.random() * 30)) * (Math.random() < 0.5 ? -1 : 1);
-    rolling = Math.max(0, Math.min(100, rolling + eccentrism));
-    bits3.push(rolling);
-  }
-
-  const li = (bit, index) => <li key={index+'_'+bit+'_'+Math.random() * 3000} style={{height: bit+'px', marginTop: (100-bit)/2}}></li>; 
 
   return (
     <div id="useless">
-      <ol className="waveform">
-        { bits.map(li) }
-      </ol>
-
-      <ol className="waveform">
-        { bits2.map(li) }
-      </ol>
-
-      <ol className="waveform">
-        { bits3.map(li) }
-      </ol>
-
-      <style jsx>{`
-      `}</style>
+      {
+        bits.map(bits => <ol className="waveform">
+          { bits.map((bit, index) => <li key={index+'_'+bit+'_'+Math.random() * 3000} style={{height: bit+'px', marginTop: (100-bit)/2}}></li>) }
+        </ol>)
+      }
 
       <style jsx global>{`
         html,
@@ -56,6 +66,17 @@ export default function Home() {
         * {
           box-sizing: border-box;
         }
+
+        ${ colors[streamCount-1].map((color, index) => {
+          return `
+            .waveform {} /* Why TF is this needed? */
+
+            .waveform:nth-child(${index+1}) li {
+              background: rgb(${color[0]},${color[1]},${color[2]});
+              background: linear-gradient(0deg, rgba(${color[0]},${color[1]},${color[2]},0) 0%, rgba(${color[0]},${color[1]},${color[2]},1) 50%, rgba(${color[0]},${color[1]},${color[2]},0) 100%); 
+            }
+          `;
+        }) }
 
         .waveform {
           margin: 0;
@@ -73,19 +94,6 @@ export default function Home() {
           float: left;
           width: 1px;
           height: 100px;
-        }
-
-        .waveform:nth-child(1) li {
-          background: rgb(255,0,0);
-          background: linear-gradient(0deg, rgba(255,0,0,0) 0%, rgba(255,0,0,1) 50%, rgba(255,0,0,0) 100%); 
-        }
-        .waveform:nth-child(2) li {
-          background: rgb(0,255,0);
-          background: linear-gradient(0deg, rgba(0,255,0,0) 0%, rgba(0,255,0,1) 50%, rgba(0,255,0,0) 100%); 
-        }
-        .waveform:nth-child(3) li {
-          background: rgb(0,0,255);
-          background: linear-gradient(0deg, rgba(0,0,255,0) 0%, rgba(0,0,255,0,1) 50%, rgba(0,0,255,0,0) 100%); 
         }
       `}</style>
     </div>
